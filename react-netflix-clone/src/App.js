f525360045b41f29ada6eba28d249d1e0bcfc848
nnevia <dbs00024@gmail.com>
2024-03-26 18:56:59 +0900

    3.26

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import requests from './api/requests';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
-  return <div className="App">
+  return <div className="app">
     <Nav />
     <Banner />
+
+    <Row 
+      title='NETFLIX ORIGINALS'
+      id='NO'
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+    />
+
+    <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+    <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+    <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+    <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+    <Footer />
+    
   </div>
 }
 
